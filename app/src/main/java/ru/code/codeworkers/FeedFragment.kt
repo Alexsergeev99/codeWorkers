3ff066d19680c341114c1fa8bdb5bbce03ec23da
Alex Sergeev <sanhokk@yandex.ru>
2024-03-27 22:43:59 +0300

    initial commit

---
+++ app/src/main/java/ru/code/codeworkers/FeedFragment.kt
@@ -0,0 +1,11 @@
+package ru.code.codeworkers
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class FeedFragment : Fragment() {
+
+}
---
+++ app/src/main/java/ru/code/codeworkers/MainActivity.kt
@@ -0,0 +1,11 @@
+package ru.code.codeworkers
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
